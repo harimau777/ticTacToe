a68daf1bc6843be7d0653de5cb23746c44b71aba
David Moneysmith <moneysmith777@gmail.com>
2016-10-18 11:38:22 -0700

    Wrote skeleton of board class

---
+++ board.js
@@ -0,0 +1,27 @@
+class Board{
+  constructor() {
+    this.grid = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']];
+    this.currentPlayer = 1;
+  }
+
+  drawBoard() {
+
+  }
+
+  checkWin() {
+
+  }
+
+  resetGame() {
+
+  }
+
+  placeMark() {
+
+  }
+
+  game() {
+
+  }
+}
+
